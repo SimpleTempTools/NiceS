7024997b29364c038f9ca0fb143ddad91e5e1367
lijinfeng <lijinfeng@360.cn>
2016-03-10 18:23:33 +0800

    add Openapi/Deploy.pm

--- lib/NS/OpenAPI/Deploy.pm
+++ lib/NS/OpenAPI/Deploy.pm
@@ -8,22 +8,44 @@ use LWP::UserAgent;
 
 use base qw( NS::OpenAPI );
 
-our %URI =
-(
-    list => '/openapi/deploy/list',
-    conf => '/openapi/deploy/conf',
-);
+our $URI = "/openapi/deploy";
 
 sub list
 {
     my $self = shift;
-    $self->get( $URI{list} );
+    $self->get( "$URI/list" );
 }
 
-sub get_conf
+sub create
 {
     my ( $self, $name ) = @_;
-    $self->get( "$URI{conf}/get/$name" );
+    $self->get( sprintf "$URI/create?name=%s", $name || $self->{name} );
+}
+
+sub main { shift->_api( 'main', @_); }
+sub mark { shift->_api( 'mark', @_); }
+sub info { shift->_api( 'info', @_); }
+
+sub conf_list { shift->_api( 'conf/', @_); }
+sub logs_list { shift->_api( 'logs/', @_); }
+
+sub conf
+{
+    my ( $self, $type ) = splice @_, 0, 2;
+    $self->_api( "conf/$type", @_);
+}
+
+sub logs
+{
+    my ( $self, $type ) = splice @_, 0, 2;
+    $self->_api( "logs/$type", @_ );
+}
+
+sub _api
+{
+    my ( $self, $type, $data ) = @_;
+    printf( "$URI/%s/$type", $self->{name});
+    $self->post( sprintf( "$URI/%s/$type", $self->{name}), $data ? ( data => $data ) :() );
 }
 
 1;

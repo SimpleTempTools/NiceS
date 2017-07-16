06dcf8d6dd58b0a241bc57fc8e2bb5559c14069d
lijinfeng2011 <lijinfeng2011@gmail.com>
2017-07-16 19:20:44 +0800

    close

--- build.pl
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env perl
-my $perl = $ENV{PERL_PATH} || $^X;
-my $nices = $perl;$nices =~ s/\/perl\/bin\/perl$//;
-exec "$perl Makefile.PL && make && make install nices=$nices && make clean";
--- nices.pl
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-rm -rf /home/work/nices/perl/lib/site_perl/5.24.0/NS
-rm -rf /home/work/nices/ns
-/home/work/nices/perl/bin/perl build.pl
